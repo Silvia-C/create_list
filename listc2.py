86ff27c025b87d36e3571ec0f2f00e547bd5c848
Silvia-C <33365850+Silvia-C@users.noreply.github.com>
2017-11-26 13:49:06 -0500

    Update listc2.py

--- listc2.py
+++ listc2.py
@@ -8,12 +8,15 @@ def add_item():
 
 def stay_choice():
     yn = input("Do you wish to save item and continue?")
-    while yn == "Y":
+    while yn == "Y" or yn == "y":
         add_item()
         yn = input("Do you wish to save item and continue?")
-    if yn == "N":
+    if yn == "N" or yn == "n":
             print("We saved your list")
             return List
+    else:
+        print("You can only enter Y or N")
+        stay_choice()
 
 add_item()
 stay_choice()
